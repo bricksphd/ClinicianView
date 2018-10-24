c64f26b87df0882495b09fdbc7ffdca6cbbb3c2e
Rosemary Keenan <rosemary.lillian.keenan@gmail.com>
2018-10-24 14:09:53 -0500

    expirimenting weith user pools

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_acHigLp2m', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4dgn27rjd7pfp7lcovbjgktvpu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
